4cc092ddaa4706e64042157a773d220ebceee825
Bachors <bachor.can@gmail.com>
2016-04-20 05:20:06 +0700

    Demo

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+	// Include compression_tester.php
+	require("compression_tester.php");
+	
+	// Call function compression_tester("URL")
+	echo compression_tester("http://ibacor.com");
+
+?>
